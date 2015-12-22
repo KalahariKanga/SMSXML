b121d8e56e7c90b32ff8f06918e18f4b413410c3
KalahariKanga <djfhill@mailinator.com>
2015-12-22 16:46:39 +0000

    message accessors

--- SMSXML/Message.h
+++ SMSXML/Message.h
@@ -1,15 +1,21 @@
 #pragma once
 #include <string>
-
+#include <chrono>
 class Message
 {
 	std::string body;
 	std::string contactName;
 	bool from;
 	long timecode;
+
 public:
 	Message();
 	Message(std::string body, std::string contact, bool from, int timecode);
 	~Message();
+
+	std::string getBody(){ return body; }
+	std::string getContactName(){ return contactName; }
+	bool getFrom(){ return from; }
+	long getTimecode(){ return timecode; }
 };
 
